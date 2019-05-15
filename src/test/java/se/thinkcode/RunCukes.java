394f64e9f58f5acff19cc349699e291d9aafd07f
Thomas Sundberg <tsu@kth.se>
2019-05-15 14:39:55 +0200

    Moved to new root package

--- src/test/java/se/thinkcode/RunCukes.java
+++ src/test/java/se/thinkcode/RunCukes.java
@@ -1,4 +1,4 @@
-package se.thinkcode.cukenfest;
+package se.thinkcode;
 
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
--- src/test/java/se/thinkcode/steps/TodoSteps.java
+++ src/test/java/se/thinkcode/steps/TodoSteps.java
@@ -1,11 +1,11 @@
-package se.thinkcode.cukenfest.steps;
+package se.thinkcode.steps;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import se.thinkcode.cukenfest.todolist.Owner;
-import se.thinkcode.cukenfest.todolist.Task;
-import se.thinkcode.cukenfest.todolist.TodoList;
+import se.thinkcode.todolist.Owner;
+import se.thinkcode.todolist.Task;
+import se.thinkcode.todolist.TodoList;
 
 import java.util.List;
 
